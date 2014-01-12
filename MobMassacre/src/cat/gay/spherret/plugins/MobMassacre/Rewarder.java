e1787082fda8af9153307a92db7e850dc7f23757
Spherretter <filip.pauzenberger@gmail.com>
2014-01-11 21:08:48 -0500

    fifth

--- MobMassacre/src/cat/gay/spherret/plugins/MobMassacre/Rewarder.java
+++ MobMassacre/src/cat/gay/spherret/plugins/MobMassacre/Rewarder.java
@@ -1,17 +1,18 @@
 package cat.gay.spherret.plugins.MobMassacre;
 
+import org.bukkit.Bukkit;
+
 public class Rewarder {
 
 	Start start = new Start();
 	GlobalVars gbvs = new GlobalVars();
-	Stats stats = new Stats();
 
 	public void rewardAll(){
 		for (String s : gbvs.rewards.keySet()){
-			int kills = stats.getKills(s);
 			int reward = gbvs.rewards.get(s);
 			String Reward = start.getConfig().getString("Reward");
 			Reward = Reward.replaceAll("@r", ((Integer) (reward)).toString()).replaceAll("@p", s);
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), Reward);
 		}
 	}
 }
