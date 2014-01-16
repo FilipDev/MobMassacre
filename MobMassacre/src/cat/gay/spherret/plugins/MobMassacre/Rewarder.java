a019854e7ce17ca4216b2bcd9f7ddedcd16dedcd
Spherretter <filip.pauzenberger@gmail.com>
2014-01-15 19:42:28 -0500

    Done. Pre-Tweaking

--- MobMassacre/src/cat/gay/spherret/plugins/MobMassacre/Rewarder.java
+++ MobMassacre/src/cat/gay/spherret/plugins/MobMassacre/Rewarder.java
@@ -18,10 +18,14 @@ public class Rewarder {
 		HashMap<String, Integer> extrapts = new HashMap<String, Integer>();
 
 		try{
-			extrapts.put(GlobalVars.entryarray[0].getKey(), GlobalVars.entryarray[0].getValue());
-			extrapts.put(GlobalVars.entryarray[1].getKey(), GlobalVars.entryarray[1].getValue());
-			extrapts.put(GlobalVars.entryarray[2].getKey(), GlobalVars.entryarray[2].getValue());
+			extrapts.put(GlobalVars.entryarray[0].getKey(), start.getConfig().getInt("placerewards.1"));
+			extrapts.put(GlobalVars.entryarray[1].getKey(), start.getConfig().getInt("placerewards.2"));
+			extrapts.put(GlobalVars.entryarray[2].getKey(), start.getConfig().getInt("placerewards.3"));
 		}catch (Exception e){}
+
+		for (String s : extrapts.keySet()){
+			System.out.println(s + " " + extrapts.get(s));
+		}
 
 		for (String s : GlobalVars.rewards.keySet()){
 			int reward = GlobalVars.rewards.get(s);
@@ -34,14 +38,19 @@ public class Rewarder {
 			String message;
 			message = start.getConfig().getString("message");
 			message = message.replaceAll("@mk", GlobalVars.rewards.get(s) + "");
-			message = message.replaceAll("@r", (reward * start.getConfig().getDouble("rate") + ""));
+			if (extrapts.containsKey(s)){
+				message = message.replaceAll("@r", ((int) (reward * start.getConfig().getDouble("rate")) + extrapts.get(s) + ""));
+			}else
+				message = message.replaceAll("@r", ((int) (reward * start.getConfig().getDouble("rate")) + ""));
 			message = message.replaceAll("@p", s);
 			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), Reward);
-			Bukkit.getPlayer(s).sendMessage(message);
+			try{
+				Bukkit.getPlayer(s).sendMessage(message);
+			}catch (NullPointerException e){}
 		}
 		GlobalVars.rewards.clear();
 		GlobalVars.statis.set("Kills", null);
-		GlobalVars.entryset.clear();
+		GlobalVars.entryset = null;
 		GlobalVars.entryarray = null;
 	}
 }
