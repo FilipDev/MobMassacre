8e60a7d0083fd271889aa1337cec4abf1eb94fcf
Spherretter <filip.pauzenberger@gmail.com>
2014-01-19 00:26:10 -0500

    Fix

--- MobMassacre/src/cat/gay/spherret/plugins/MobMassacre/Events.java
+++ MobMassacre/src/cat/gay/spherret/plugins/MobMassacre/Events.java
@@ -1,16 +1,10 @@
 package cat.gay.spherret.plugins.MobMassacre;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.entity.Entity;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.*;
 
 public class Events implements Listener{
 
